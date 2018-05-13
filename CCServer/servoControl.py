58086551d07cdddfe7d0a2f6b7e5b4e408fe6d5d
Bahar Şengez <33401276+Baharsengezz@users.noreply.github.com>
2018-05-13 22:28:27 +0300

    ServoControl.py

--- CCServer/servoControl.py
+++ CCServer/servoControl.py
@@ -2,13 +2,15 @@ from gpiozero import AngularServo
 from time import sleep
 
 #Servo motor X direction initialization
-servoX = AngularServo(17,min_angle=45, max_angle= -45)
-#servoY = AngularServo(18,min_angle=90, max_angle=-90)
+servoX = AngularServo(17,min_angle=-45, max_angle= 45)
+servoY = AngularServo(18,min_angle=45, max_angle=-45)
 
 
 #When CuriosCam Start, Fix Servo Motors in MidPosition
 def servoMid():
     servoX.mid()
+    sleep(0.5)
+    servoY.angle = 25
     sleep(0.5)
     #servoY.mid()
     #sleep(0.1)
@@ -22,51 +24,105 @@ def servoMid():
 ##    centerY = int(resY)/2
 ##    print("Center X:",centerX,"Center Y:",centerY)
     
-def controlServo(faceX,faceY,centerX,centerY):
-    print("ROTATE")
+def controlServoX(faceX,centerX):
+    faceX = centerX*2-float(faceX)
     disX = centerX - float(faceX)
-    disY = centerY - float(faceY)
+    print("Face",faceX,"Disx",disX)
     
-    disAngleX = (45/centerX) * disX
-    #print(disAngleX)
+    disAngleX = (22.5/centerX) * disX
     
-    if servoX.angle == 0:
-        servoX.angle = disAngleX
-        sleep(0.5)
-        print(servoX.angle)
-    elif servoX.angle >= 0 and disAngleX >= 0:
-        if servoX.angle + disAngleX <= 45:
-            servoX.angle = servoX.angle + disAngleX
-            sleep(0.5)
-            print(servoX.angle)
-        else:
-            servoX.min()
-            sleep(0.5)
-            print('min')
-    elif servoX.angle >= 0 and disAngleX < 0:
-        servoX.angle = servoX.angle + disAngleX
-        sleep(0.5)
-        print(servoX.angle)
-    elif servoX.angle < 0:
-        if servoX.angle + disAngleX >= -45:
-            servoX.angle = servoX.angle + disAngleX
-            sleep(0.5)
-            print(servoX.angle)
+    print("Angle X",disAngleX)
+    
+    #mirror of face x
+    if disAngleX < centerX:
+        disAngleX = -disAngleX
+    
+    
+    #max x
+    if servoX.angle >= 0:
+        if servoX.angle+disAngleX <= 45:
+            servoX.angle = servoX.angle+disAngleX
+            sleep(1)
+            print("Max angle X =",servoX.angle)
         else:
             servoX.max()
-            sleep(0.5)
-            print('max')
+            sleep(1)
+            print("Max angle X =",servoX.angle)
+    #min x
+    elif servoX.angle <= 0:
+        if servoX.angle+disAngleX >= -45:
+            servoX.angle = servoX.angle+disAngleX
+            sleep(1)
+            print("Min angle =",servoX.angle)
+        else:
+            servoX.min()
+            sleep(1)
+            print("Min angle =",servoX.angle)
+
+def controlServoY(faceY,centerY):
+    
+    faceY = centerY*2-float(faceY)
+    disY = centerY - float(faceY) 
+    disAngleY = (22.5/centerY) * disY
+    print("Angle Y",disAngleY)
+    #mirror of face y
+    if disAngleY < centerY:
+        disAngleY = -disAngleY
+    #min y
+    if servoY.angle >= 0:
+        if servoY.angle + disAngleY <=45:
+            servoY.angle = servoY.angle+disAngleY
+            sleep(1)
+            print("Min angle Y =",servoY.angle)
+        else:
+            servoY.min()
+            sleep(1)
+            print("Min angle Y =",servoY.angle)
+    #max y
+    elif servoY.angle <= 0:
+        if servoY.angle+disAngleY >= -45:
+            servoY.angle = servoY.angle + disAngleY
+            sleep(1)
+            print("Max angle Y =",servoY.angle)
+        else:
+            servoY.max()
+            sleep(1)
+            print("Max angle Y =",servoY.angle)
+    
         
 
-def turnRight():
-    if(servoX.angle > -45):
+def turnLeft():
+    if(servoX.angle - 15 > -45):
         servoX.angle = servoX.angle - 15
         sleep(0.5)
+    else:
+        servoX.min()
+        sleep(0.5)
 
-def turnLeft():
-    if(servoX.angle < 45):
+def turnRight():
+    if(servoX.angle + 15 < 45):
         servoX.angle = servoX.angle +15
         sleep(0.5)
+    else:
+        servoX.max()
+        sleep(0.5)
+
+def turnUp():
+    if(servoY.angle +15 <= 45):
+        servoY.angle = servoY.angle +15
+        sleep(0.5)
+    else:
+        servoY.min()
+        sleep(0.5)
+    
+def turnDown():
+    if(servoY.angle -15 >= -45):
+        servoY.angle = servoY.angle - 15
+        sleep(0.5)
+    else:
+        servoY.max()
+        sleep(0.5)
+    
 
 
     
